ba2119555f27742eea74054cca03d78c2f246fe7
ciszakdamian <ciszakdamian@gmail.com>
2020-01-31 19:43:21 +0100

    add load model from file

--- model.py
+++ model.py
@@ -6,6 +6,8 @@ import os
 
 
 class Model:
+    path = 'models'
+
     def __init__(self, features, labels):
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(features, labels, test_size=0.1,
                                                                                 random_state=0)
@@ -13,25 +15,37 @@ class Model:
         self.model.fit(self.x_train, self.y_train)
 
     def test(self):
-        print('\nModel test: ')
         y_pred = self.model.predict(self.x_test)
         df = pd.DataFrame({'Actual': self.y_test, 'Predicted': y_pred})
-        print(df)
+        return df
 
     def dump(self, name):
-        path = 'models'
-        if not os.path.exists(path):
-            os.makedirs(path)
-        pickle.dump(self.model, open(path + '/' + name, 'wb'))
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        pickle.dump(self.model, open(self.path + '/' + name, 'wb'))
+        info = 'Model dump to: ' + self.path + '/' + name
+        return info
 
-# wczytanie danych
-dataset = pd.read_csv('2019-nCoV.csv')
+    def load(self, name):
+        try:
+            m = open(self.path + '/' + name)
+            model = pickle.load(open(self.path + '/' + name, 'rb'))
+            return model
+        except IOError:
+            print('Error: Model not exist ' + self.path + '/' + name)
 
-# usuniecie zbednych danych i skopiowanie etykiety
-f = dataset.drop(['zgony', 'data'], axis=1)
-l = dataset['zgony']
 
-deaths = Model(f, l)
-
-deaths.test()
-deaths.dump('zgony.p')
+# # wczytanie danych
+# dataset = pd.read_csv('2019-nCoV.csv')
+#
+# # usuniecie zbednych danych i skopiowanie etykiety
+# f = dataset.drop(['zgony', 'data'], axis=1)
+# l = dataset['zgony']
+#
+# deaths = Model(f, l)
+#
+# deaths.test()
+#
+# print(deaths.dump('deaths.p'))
+#
+# print(deaths.load('deaths.p'))
